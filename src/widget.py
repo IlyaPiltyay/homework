e4b8f1dd79e1217310d54be5867b12c59f09c10b
Ilya <ilya.piltyay@gmail.com>
2024-07-08 14:02:09 +0300

    new_f

--- src/widget.py
+++ src/widget.py
@@ -1,21 +1,9 @@
-import os
+import datetime
 
-from src.masks import mask_account,mask_card
+from src.masks import mask_account, mask_card
 
-
-def mask_account_cart(type_and_number_cart: str) -> str:
-
-
-current_directory = os.getcwd()
-print(current_directory)
-
-if __name__ == "__main__":
-    print(mask_card("7000792289606361"))
-    print(mask_account("73654108430135874305"))
-
-
-cart_and_account_numbers = """ Maestro 1596837868705199
-Счет 64686473678894779589
+example_data = """ Maestro 1596837868705199
+Счет 64686473678894779589#
 MasterCard 7158300734726758
 Счет 35383033474447895560
 Visa Classic 6831982476737658
@@ -23,4 +11,22 @@ Visa Platinum 8990922113665229
 Visa Gold 5999414228426353
 Счет 73654108430135874305 """
 
-date = "2018-07-11T02:26:18.671407"
+
+def process_input(input_str: str) -> str:
+    if "Счет" in input_str:
+        return mask_account(input_str.split()[1])
+    else:
+        new_card = mask_card(input_str.split()[-1])
+        return input_str.replace(input_str.split()[-1], new_card)
+
+
+if __name__ == "__main__":
+    print(process_input("Maestro 1596837868705199"))
+    print(process_input("MasterCard 7158300734726758"))
+    print(process_input("Visa Classic 6831982476737658"))
+    print(process_input("Visa Platinum 8990922113665229"))
+    print(process_input("Visa Gold 5999414228426353"))
+    print(process_input("Счет 73654108430135874305"))
+    print(process_input("Счет 64686473678894779589"))
+    print(process_input("Счет 35383033474447895560"))
+    print(datetime.datetime(2024, 3, 11))
